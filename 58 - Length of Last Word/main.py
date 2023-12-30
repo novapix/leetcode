f332f16dda266cae0b2b41a60145a11f05337aa4
Parakram <89983841+Parakramk@users.noreply.github.com>
2023-12-30 14:58:23 +0545

    58, 70

--- 58 - Length of Last Word/main.py
+++ 58 - Length of Last Word/main.py
@@ -1,9 +1,10 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         s = s.rstrip()
-        last = s.split(" ")[-1]
+        last = s.rsplit(" ", maxsplit=1)[-1]
         return len(last)
 
 
 sol = Solution()
 print(sol.lengthOfLastWord("   afwf    abfwe0f fer         "))
+print(sol.lengthOfLastWord("a"))
